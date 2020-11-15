601953eea49aa90c757340cdb6e20baa3dabe22d
Platonov2 <jek1999060@mail.ru>
2020-11-15 13:38:30 +0500

    Теперь CORS работает корректно

--- crm_backend/src/main/java/com/ues/crm_backend/Config/CorsConfig.java
+++ crm_backend/src/main/java/com/ues/crm_backend/Config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8080");
+            .allowedOrigins("http://localhost:8080")
+            .allowedMethods("*");
     }
 }
